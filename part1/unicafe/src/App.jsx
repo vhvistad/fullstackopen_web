6b8e21f1d3032b74e7d81dcc243c85e000a93ddf
VISTAD Vegard <vegard.vistad@soprasteria.com>
2025-01-10 14:03:46 +0100

    Part 1d step 2

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -3,19 +3,22 @@ import { useState } from 'react'
 const Button = (props) => {
   return (
     <button onClick={props.handleClick}>
-      {props.feedback}
+      {props.text}
     </button>
   )
 }
 
 const Statistics = (props) => {
-  const [good, neutral, bad] = props.stats
+  const [good, neutral, bad, total, avg, posPercent] = props.stats
   console.log(good)
   return (
     <div>
       <div>Good: {good}</div>
       <div>Neutral: {neutral}</div>
       <div>Bad: {bad}</div>
+      <div>All: {total}</div>
+      <div>Average: {avg}</div>
+      <div>Positive: {posPercent} %</div>
     </div>
   )
 }
@@ -25,15 +28,40 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [total, setTotal] = useState(0)
+  const [avg, setAvg] = useState(0)
+  const [posPercent, setPosPercent] = useState(0)
+
+  const registerFeedback = (feedback) => {
+    let newGood = good
+    let newNeutral = neutral
+    let newBad = bad
+    let newTotal = total + 1
+
+    if (feedback === 'good') {
+      newGood++
+      setGood(newGood)
+    }
+    if (feedback === 'neutral') {
+      newNeutral++
+      setNeutral(newNeutral)
+    }
+    if (feedback === 'bad') {
+      newBad++
+      setBad(newBad)
+    }
+
+    setTotal(newTotal)
+    setAvg((newGood - newBad) / newTotal)
+    setPosPercent(newGood / newTotal * 100)
+  }
 
   return (
     <div>
-      <div>
-        <Button handleClick={() => setGood(good + 1)} feedback='good'/>
-        <Button handleClick={() => setNeutral(neutral + 1)} feedback='neutral'/>
-        <Button handleClick={() => setBad(bad + 1)} feedback='bad'/>
-      </div>
-      <Statistics stats={[good, neutral, bad]}/>
+      <Button handleClick={() => registerFeedback('good')} text='good'/>
+      <Button handleClick={() => registerFeedback('neutral')} text='neutral'/>
+      <Button handleClick={() => registerFeedback('bad')} text='bad'/>
+      <Statistics stats={[good, neutral, bad, total, avg, posPercent]}/>
     </div>
   )
 }
